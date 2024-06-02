5a10b6dbbf3fc9ba56ba797aa665a769a2f8b15f
wsb777 <a.sizov.wsb@gmail.com>
2024-06-03 00:07:33 +0300

    documentation

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,20 @@
 import './scss/styles.scss';
 
+type Category = 'софт-скил' | 'другое' | 'дополнительное' | 'кнопка' | 'хард-скил';
+
+type PaymentType = 'Онлайн' | 'При получении';
+
+interface IProduct {
+    name: string;
+    category: Category;
+    description: string;
+    image: ImageData;
+    price: number;
+};
+
+interface IOrder {
+    paymentTyoe: PaymentType;
+    address: string;
+    email: RegExp;
+    phone: RegExp;
+};
